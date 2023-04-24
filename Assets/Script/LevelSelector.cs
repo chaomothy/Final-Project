81470ec14783d4760b9891ecaacb1b290a06e489
Sam Rocha <124705709+chaomothy@users.noreply.github.com>
2023-04-24 14:04:47 -0400

    TITLE SCREEN + LEVEL SELECT
    Added a title screen as well as a level selector that allows the player to select any level at any time (plus the sandbox). This will be expanded upon later with a fade-in and fade-out when whenever shifting between scenes to make it look nicer.

---
+++ Assets/Script/LevelSelector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    
+    public int level;
+
+    public void OpenScene()
+    {
+    
+        SceneManager.LoadScene("Level " + level.ToString());
+
+    }
+
+    public void OpenSandbox()
+    {
+    
+        SceneManager.LoadScene(2);
+
+    }
+
+}
---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+    
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void LevelSelect() 
+    {
+    
+        SceneManager.LoadScene(1);
+
+    }
+    
+    public void QuitGame()
+    {
+    
+        Application.Quit();
+    
+    }
+
+}
